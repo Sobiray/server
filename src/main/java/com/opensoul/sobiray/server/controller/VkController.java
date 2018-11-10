f4c018f5562177c2050ac078d0f5737d1f788b8b
jajul <jajulnex@mail.ru>
2018-11-10 16:02:30 +0300

    test data + heroku

--- src/main/java/com/opensoul/sobiray/server/controller/VkController.java
+++ src/main/java/com/opensoul/sobiray/server/controller/VkController.java
@@ -1,6 +1,7 @@
 package com.opensoul.sobiray.server.controller;
 
 import com.opensoul.sobiray.server.utils.ContractHelper;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class VkController {
@@ -16,31 +19,46 @@ public class VkController {
 
     @RequestMapping("/hi")
     public String hi(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return "Hello, " + name;
+        return "{\"result\":\"" + "Hello, " + name + "!" + "\"}";
     }
 
-    @RequestMapping("/newPayment")
-    public boolean newPayment(@RequestParam(value = "from") String from,
-                              @RequestParam(value = "to") String to,
-                              @RequestParam(value = "amount") BigDecimal amount) {
+    @RequestMapping("/payment")
+    public boolean payment(@RequestParam(value = "from") String from,
+                           @RequestParam(value = "to") String to,
+                           @RequestParam(value = "amount") BigDecimal amount) {
         return ContractHelper.createNewPayment(from, to, amount);
     }
 
-    @RequestMapping("/eventList")
-    public List<String> eventList() {
+    @RequestMapping("/events")
+    public String events() {
         if (eventList == null) {
             eventList = ContractHelper.getEventList();
+            eventList = Stream.of("2", "fd", "sfs").collect(Collectors.toList());
         }
-        return eventList;
+        StringBuffer result = new StringBuffer("[");
+        int i = 0;
+        for (String event : eventList) {
+            result.append(i++ == 0 ? "{" : ",{");
+            result.append(" \"id\": 1,\n" +
+                    "    \"preorderPrice\": 150,\n" +
+                    "    \"price\": 300,\n" +
+                    "    \"deadline\": \"27.01.2018\",\n" +
+                    "    \"date\": \"15.02.2018\",\n" +
+                    "    \"requiredMoney\": 15000,\n" +
+                    "    \"maxGuests\": 50");
+            result.append("}");
+        }
+        result.append("]");
+        return result.toString();
     }
 
-    @RequestMapping("/newEvent")
-    public boolean newEvent(@RequestParam(value = "eventId") String eventId,
-                            @RequestParam(value = "successSum") BigDecimal successSum,
-                            @RequestParam(value = "maxGuestsCount") int maxGuestsCount,
-                            @RequestParam(value = "presalePrice") BigDecimal presalePrice,
-                            @RequestParam(value = "salePrice") BigDecimal salePrice,
-                            @RequestParam(value = "lastPresaleDate") LocalDateTime lastPresaleDate) {
+    @RequestMapping("/event")
+    public boolean event(@RequestParam(value = "eventId") String eventId,
+                         @RequestParam(value = "successSum") BigDecimal successSum,
+                         @RequestParam(value = "maxGuestsCount") int maxGuestsCount,
+                         @RequestParam(value = "presalePrice") BigDecimal presalePrice,
+                         @RequestParam(value = "salePrice") BigDecimal salePrice,
+                         @RequestParam(value = "lastPresaleDate") LocalDateTime lastPresaleDate) {
         return ContractHelper.createNewEvent(eventId, successSum, maxGuestsCount, presalePrice, salePrice, lastPresaleDate);
     }
 }
